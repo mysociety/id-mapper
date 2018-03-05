34dac7d78c579d2ad3f559df6515b6d8b712405d
Graeme Porteous <graeme@rgbp.co.uk>
2018-03-05 10:22:42 +0000

    Initialise scheme objects
      `IDMapper.scheme('scheme-name')

--- lib/id_mapper.rb
+++ lib/id_mapper.rb
@@ -4,7 +4,15 @@ require_relative './id_mapper/request'
 require_relative './id_mapper/scheme'
 
 module IDMapper
+  InvalidScheme = Class.new(ArgumentError)
+
   def self.schemes
     Request.get('scheme')[:results].map { |r| Scheme.new(r) }
+  end
+
+  def self.scheme(scheme_name)
+    scheme = schemes.find { |s| s.name == scheme_name }
+    raise InvalidScheme, 'scheme no present' unless scheme
+    scheme
   end
 end
--- test/id_mapper_test.rb
+++ test/id_mapper_test.rb
@@ -18,4 +18,19 @@ describe IDMapper do
       assert_equal 'uk-area_id', schemes.first.name
     end
   end
+
+  describe '.scheme' do
+    it 'raises error when scheme is invalid' do
+      assert_raises(IDMapper::InvalidScheme) do
+        IDMapper.scheme(scheme: 'invalid')
+      end
+    end
+
+    it 'returns scheme with given name' do
+      scheme = IDMapper.scheme('uk-area_id')
+      assert_instance_of IDMapper::Scheme, scheme
+      assert_equal 1, scheme.id
+      assert_equal 'uk-area_id', scheme.name
+    end
+  end
 end
