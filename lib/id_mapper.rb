478f7b2afe9673d2eeac683bf0d27a63923b2eb8
Graeme Porteous <graeme@rgbp.co.uk>
2018-03-07 15:37:02 +0000

    Add class level methods to `class << self` block

--- lib/id_mapper.rb
+++ lib/id_mapper.rb
@@ -7,13 +7,15 @@ require_relative './id_mapper/scheme'
 module IDMapper
   InvalidScheme = Class.new(ArgumentError)
 
-  def self.schemes
-    Request.get('scheme')[:results].map { |r| Scheme.new(r) }
-  end
+  class << self
+    def schemes
+      Request.get('scheme')[:results].map { |r| Scheme.new(r) }
+    end
 
-  def self.scheme(scheme_name)
-    scheme = schemes.find { |s| s.name == scheme_name }
-    raise InvalidScheme, 'scheme no present' unless scheme
-    scheme
+    def scheme(scheme_name)
+      scheme = schemes.find { |s| s.name == scheme_name }
+      raise InvalidScheme, 'scheme no present' unless scheme
+      scheme
+    end
   end
 end
